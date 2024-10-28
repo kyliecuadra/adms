9ba5339e1c9a5d4f222d2cee819bb5b2611f66b9
Kylie Cuadra <45311838+kyliecuadra@users.noreply.github.com>
2024-10-28 23:35:31 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -10,7 +10,7 @@ function userExists($conn, $email) {
 // Main registration logic
 // if (isset($_POST['fname'], $_POST['mname'], $_POST['lname'], $_POST['program'],
 //         $_POST['department'], $_POST['email'], $_POST['cnumber'], $_POST['password'])) {
-if (isset($_POST['fname'], $_POST['mname'], $_POST['lname'], $_POST['email'], $_POST['cnumber'])) {
+if (isset($_POST['fname'], $_POST['mname'], $_POST['lname'], $_POST['email'], $_POST['cnumber'], $_POST['password'])) {
 
     $fname = $_POST['fname'];
     $mname = $_POST['mname'];
@@ -19,16 +19,12 @@ if (isset($_POST['fname'], $_POST['mname'], $_POST['lname'], $_POST['email'], $_
     // $department = $_POST['department'];
     $email = $_POST['email'];
     $cnumber = $_POST['cnumber'];
-    //$password = $_POST['password'];
-
-    $password = $_SESSION["fname"][0].$_SESSION["lname"];
-    $password = str_replace(' ', '', $password);
-    $password = mb_strtoupper($password);
+    $password = $_POST['password'];
 
     if (!userExists($conn, $email)) {
         // Using plain mysqli to insert data
         //$query = "INSERT INTO users (name, program, department, phoneNumber, email, password, user_level) VALUES ('$name', '$program', '$department', '$cnumber', '$email', '$password', 'quaac')";
-        $query = "INSERT INTO users (fname, mname, lname, phoneNumber, email, password, role, status) VALUES ('$fname', '$mname.', '$lname', '$cnumber', '$email', '$password', 'quaac', 'unregistered')";
+        $query = "INSERT INTO users (fname, mname, lname, phoneNumber, email, password, role, status) VALUES ('$fname', '$mname.', '$lname', '$cnumber', '$email', '$password', 'quaac', 'inactive')";
         if (mysqli_query($conn, $query)) {
             echo 'success'; // Success response
         } else {
